7b605565675c7e1cb5f22431a0b1795f50fbdb21
Shail Desai <shailrd2@illinois.edu>
2020-12-13 14:02:22 -0800

    classify

--- classify.py
+++ classify.py
@@ -10,7 +10,7 @@ non_sarcasm_tokens = {}
 total_sarcasm_tokens = 0
 total_non_sarcasm_tokens = 0
 
-smoothing_parameter = 0.0000001
+smoothing_parameter = 0.2
 pos_prior = 0.78
 
 response_list = []
